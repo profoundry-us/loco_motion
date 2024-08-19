4d5e8a3e4fb1346d0965201ea3378fda3bb1edf7
Alexander Burgos <alexb@Alexanders-Laptop.local>
2024-08-19 13:26:54 -0500

    Modified table code. Work in progress.

--- app/components/daisy/data_display/table_component.rb
+++ app/components/daisy/data_display/table_component.rb
@@ -1,14 +1,12 @@
 class Daisy::DataDisplay::TableComponent < LocoMotion.configuration.base_component_class
   set_component_name :table
 
-  define_parts :table_head, :table_body, :header, :row, :wrapper
-
-  renders_one :component
+  define_parts :head, :body, :header, :row, :wrapper
   
   def initialize(*args, **kws, &block)
     super
 
-    set_tag_name(:component)
+    set_tag_name(:component, :table)
   end
 
   def before_render
@@ -20,6 +18,5 @@ class Daisy::DataDisplay::TableComponent < LocoMotion.configuration.base_compone
   def setup_component
     add_css(:wrapper, "overflow-x-auto")
     add_css(:component, "table")
-    add_html(:component, "table")
   end
 end
