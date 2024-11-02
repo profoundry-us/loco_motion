5a69a6b60b45943b27e220b583e2ea8e111424e8
Topher Fangio <topher@profoundry.us>
2024-11-02 16:51:43 -0500

    Fix bug with API docs redirect.

--- docs/demo/app/controllers/api_docs_controller.rb
+++ docs/demo/app/controllers/api_docs_controller.rb
@@ -3,7 +3,9 @@ class ApiDocsController < ApplicationController
   def index
     host = ENV.fetch("LOCO_DOCS_HOST", "http://localhost:8808/docs")
 
-    redirect_to host
+    # We specifically want to allow another host because the prod docs are on
+    # a different URL
+    redirect_to host, allow_other_host: true
   end
 
 end
