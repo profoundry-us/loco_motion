d7fe28617f613b7ddf11a3fa57e3ccfb5792da9b
Topher Fangio <topher@profoundry.us>
2025-01-13 11:24:29 -0600

    Updated button API docs.

--- app/components/daisy/actions/button_component.rb
+++ app/components/daisy/actions/button_component.rb
@@ -2,9 +2,9 @@
 # The Button component can be used to render HTML `<button>` or `<a>` elements
 # that are styled to look like a clickable element.
 #
-# Note we do **not** use component parts for the icons since we're calling
-# `heroicon_tag` within the component. But we do provide custom CSS & HTML
-# options to allow overriding.
+# Note that we do **not** use component parts for the icons since we're calling
+# `heroicon_tag` within the component. But we **do** provide custom CSS & HTML
+# options to allow overriding / customization.
 #
 # Includes the {LocoMotion::Concerns::TippableComponent} module to enable easy
 # tooltip addition.
@@ -22,40 +22,75 @@
 class Daisy::Actions::ButtonComponent < LocoMotion::BaseComponent
   prepend LocoMotion::Concerns::TippableComponent
 
-  set_component_name :btn
-
   #
   # Instantiate a new Button component.
   #
-  # @param title [String] The title of the button. Defaults to `Submit` if none
-  #   of title, left icon, or right icon is provided.
-  # @param kws  [Hash] The keyword arguments for the component.
+  # @param title  [String] The title of the button. Defaults to "Submit" if none
+  #   of title, left icon, or right icon is provided. Will be considered the
+  #   `action` parameter if **both** the title and a block are provided.
   #
-  # @option kws href            [String] A path or URL to which the user will be
-  #   directed when the button is clicked. Forces the Button to use an `<a>` tag.
-  # @option kws target          [String] The HTML `target` of for the `<a>` tag
-  #   (`_blank`, `_parent`, or a specific tab / window / iframe, etc).
-  # @option kws icon            [String] The name of Hero icon to render inside
-  #   the button. This is an alias of `left_icon`.
-  # @option kws icon_css        [String] The CSS classes to apply to the icon.
-  #   This is an alias of `left_icon_css`.
-  # @option kws icon_html       [Hash] Additional HTML attributes to apply to the
-  #   icon. This is an alias of `left_icon_html`.
-  # @option kws left_icon       [String] The name of Hero icon to render inside
-  #   the button to the left of the text.
-  # @option kws left_icon_css   [String] The CSS classes to apply to the icon.
-  # @option kws left_icon_html  [Hash] Additional HTML attributes to apply to
-  #   the icon.
-  # @option kws right_icon      [String] The name of Hero icon to render inside
-  #   the button to the right of the text.
-  # @option kws right_icon_css  [String] The CSS classes to apply to the icon.
-  # @option kws right_icon_html [Hash] Additional HTML attributes to apply to
-  #   the icon.
+  # @param action [String] The Stimulus action that should fire when the button
+  #   is clicked.
+  #
+  # @param kws    [Hash] The keyword arguments for the component.
+  #
   # @option kws title           [String] The title of the button. You can also
   #   pass the title, icons, or any other HTML content as a block.
   #
-  def initialize(title = nil, **kws, &block)
+  # @option kws action          [String] The Stimulus action that should fire
+  #   when the button is clicked.
+  #
+  #   > **Note:** _You should use either the `action` or the `href` option, but
+  #   not both._
+  #
+  # @option kws href            [String] A path or URL to which the user will be
+  #   directed when the button is clicked. Forces the Button to use an `<a>`
+  #   tag.
+  #
+  #   > **Note:** _You should use either the `action` or the `href` option, but
+  #   not both._
+  #
+  # @option kws target          [String] The HTML `target` of for the `<a>` tag
+  #   (`_blank`, `_parent`, or a specific tab / window / iframe, etc).
+  #
+  # @option kws icon            [String] The name of Hero icon to render inside
+  #   the button.  This is an alias of `left_icon`.
+  #
+  # @option kws icon_css        [String] The CSS classes to apply to the icon.
+  #   This is an alias of `left_icon_css`.
+  #
+  # @option kws icon_html       [Hash] Additional HTML attributes to apply to
+  #   the icon. This is an alias of `left_icon_html`.
+  #
+  # @option kws left_icon       [String] The name of Hero icon to render inside
+  #   the button to the left of the text.
+  #
+  # @option kws left_icon_css   [String] The CSS classes to apply to the left
+  #   icon.
+  #
+  # @option kws left_icon_html  [Hash] Additional HTML attributes to apply to
+  #   the left icon.
+  #
+  # @option kws right_icon      [String] The name of Hero icon to render inside
+  #   the button to the right of the text.
+  #
+  # @option kws right_icon_css  [String] The CSS classes to apply to the right
+  #   icon.
+  #
+  # @option kws right_icon_html [Hash] Additional HTML attributes to apply to
+  #   the right icon.
+  #
+  def initialize(title = nil, action = nil, **kws, &block)
     super
+
+    # If both a title and a block are provided, assume the title is the action
+    action = title if title && block_given?
+
+    # Force the title to be nil if a block is given so we don't accidentally
+    # render two titles
+    title = nil if block_given?
+
+    @action = config_option(:action, action)
 
     @href = config_option(:href)
     @target = config_option(:target)
