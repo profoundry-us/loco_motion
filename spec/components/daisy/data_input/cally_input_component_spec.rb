b80f43e1d2d6850a9d7cdbc14ed681e6b40279f7
Topher Fangio <topher@profoundry.us>
2025-05-15 10:51:38 -0500

    Fix a failing spec and a debug message.

--- spec/components/daisy/data_input/cally_input_component_spec.rb
+++ spec/components/daisy/data_input/cally_input_component_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe Daisy::DataInput::CallyInputComponent, type: :component do
   it "renders basic component" do
     render_inline(described_class.new)
-    expect(page).to have_css(".cally-input")
+    expect(page).to have_css("input.input")
+    expect(page).to have_css("[popover='auto'] calendar-date")
   end
 end
