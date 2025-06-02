a6597d513c83ed4e158bbd91b9ccce6c1c2de6ad
Topher Fangio <topher@profoundry.us>
2025-06-02 11:41:17 -0500

    Refactor modal specs to be more readable.

--- docs/demo/e2e/daisy/actions/modals.spec.ts
+++ docs/demo/e2e/daisy/actions/modals.spec.ts
@@ -1,6 +1,9 @@
-import { expect, test } from '@playwright/test';
+import { expect, test, Locator } from '@playwright/test';
 import { loco } from '../../spec_helpers';
 
+/**
+ * Verify the Modals page loads correctly with expected elements
+ */
 test('page loads', async ({ page }) => {
   await page.goto('/');
 
@@ -15,61 +18,56 @@ test('page loads', async ({ page }) => {
   ]);
 });
 
-test('opens modal', async ({ page }) => {
-  await page.goto('/examples/Daisy::Actions::ModalComponent');
+/**
+ * Test suite for the simple modal functionality
+ */
+test.describe('simple modal', () => {
+  let modal: Locator;
 
-  const dialog = await page.locator('dialog.modal').filter({ has: page.getByRole('heading', { name: 'Simple Modal' }) })
+  // Visit the page, verify modal is hidden, open modal
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/examples/Daisy::Actions::ModalComponent');
 
-  // Click the Simple Modal button
-  await page.click('button:has-text("Open Modal")');
+    modal = await page.locator('dialog.modal').filter({ has: page.getByRole('heading', { name: 'Simple Modal' }) })
 
-  // Expect the modal to be open
-  await expect(dialog).toBeVisible();
+    // Expect the modal to start out closed
+    await expect(modal).toBeHidden();
+
+    // Open the modal
+    await page.click('button:has-text("Open Modal")');
+  })
+
+  /**
+   * Verify the modal opens correctly
+   */
+  test('opens modal', async () => {
+    // Expect the modal to be open
+    await expect(modal).toBeVisible();
+  })
+
+  /**
+   * Verify the modal closes when Cancel button is clicked
+   */
+  test('closes modal', async ({ page }) => {
+    // Click the Close button
+    await page.click('button:has-text("Cancel")');
+
+    // Expect the modal to be closed
+    await expect(modal).toBeHidden();
+  })
+
+  /**
+   * Verify the modal submits correctly and displays confirmation
+   */
+  test('submits modal', async ({ page }) => {
+    // Click the Submit button
+    await page.click('button:has-text("Submit")');
+
+    // Expect the modal to be closed
+    await expect(modal).toBeHidden();
+
+    // Expect the page to have the submission text
+    await expect(page.getByText('You submitted the modal!', { exact: true })).toBeVisible();
+  })
+
 })
-
-test('closes modal', async ({ page }) => {
-  await page.goto('/examples/Daisy::Actions::ModalComponent');
-
-  const dialog = await page.locator('dialog.modal').filter({ has: page.getByRole('heading', { name: 'Simple Modal' }) })
-
-  // Expect the modal to start out closed
-  await expect(dialog).toBeHidden();
-
-  // Click the Simple Modal button
-  await page.click('button:has-text("Open Modal")');
-
-  // Expect the modal to be open
-  await expect(dialog).toBeVisible();
-
-  // Click the Close button
-  await page.click('button:has-text("Cancel")');
-
-  // Expect the modal to be closed
-  await expect(dialog).toBeHidden();
-})
-
-
-test('submits modal', async ({ page }) => {
-  await page.goto('/examples/Daisy::Actions::ModalComponent');
-
-  const dialog = await page.locator('dialog.modal').filter({ has: page.getByRole('heading', { name: 'Simple Modal' }) })
-
-  // Expect the modal to start out closed
-  await expect(dialog).toBeHidden();
-
-  // Click the Simple Modal button
-  await page.click('button:has-text("Open Modal")');
-
-  // Expect the modal to be open
-  await expect(dialog).toBeVisible();
-
-  // Click the Submit button
-  await page.click('button:has-text("Submit")');
-
-  // Expect the modal to be closed
-  await expect(dialog).toBeHidden();
-
-  // Expect the page to have the submission text
-  await expect(page.getByText('You submitted the modal!', { exact: true })).toBeVisible();
-})
-
