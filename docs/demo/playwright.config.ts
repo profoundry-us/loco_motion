d0e0f22380fd0c2c32626b4fa3114963af9a2b75
Topher Fangio <topher@profoundry.us>
2025-06-02 09:47:36 -0500

    Add vidoes for CI

--- docs/demo/playwright.config.ts
+++ docs/demo/playwright.config.ts
@@ -30,6 +30,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    /* Collect all videos on CI */
+    video: process.env.CI ? 'on' : 'off',
   },
 
   /* Configure projects for major browsers */
